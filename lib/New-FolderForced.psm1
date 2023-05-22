3b43ad92abd28bce40b686fb4d7773522e9d2284
Soham Ray <102452424+sysmonyx@users.noreply.github.com>
2023-05-22 23:57:57 +0530

    Add files via upload

---
+++ lib/New-FolderForced.psm1
@@ -0,0 +1,21 @@
+################################################################################################################################
+#   Description:
+#
+#   Last Updated:   
+################################################################################################################################
+function New-FolderForced {
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+		[Parameter(Position = 0, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+		[string]
+        $Path
+    )
+
+    process {
+        if (-not (Test-Path $Path)) {
+            Write-Verbose "-- Creating full path to:  $Path"
+            New-Item -Path $Path -ItemType Directory -Force
+        }
+    }
+}
+################################################################################################################################
